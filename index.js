5e5b877d4ee65ae5e3b56e4fbb2a996cadd3f1e6
peter <pngugi.wainaina@gmail.com>
2023-09-24 15:30:03 +0300

    parameter lab commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(wainaina){
+    return(`Hi, my name is ${wainaina}.`);
+}
+function introductionWithLanguage(wainaina,Javascript){
+    return(`Hi, my name is ${wainaina} and I am learning to program in ${Javascript}.`);
+}
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
